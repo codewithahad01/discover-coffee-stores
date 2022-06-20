41f1f40c54fccc97abaaa4450d7f92693d674c38
= <=>
2022-06-20 20:34:57 +0430

    tailwindcss configured for this project

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1 className='${styles.title} text-orange-600 text-4xl'>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
