8e95cdc824657c3b7675160f253ee11a2658921e
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-06-21 20:50:04 +0430

    banner section modified

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Banner(props) {
     return (
-        <div className='flex flex-col mx-auto space-x-2 p-6 '>
+        <div className='mx-auto space-x-2 p-6 justify-center items-center '>
             <h1 className='text-3xl text-white font-bold'>
                 <span className='px-1'>Coffee</span>
                 <span className='text-violet-900'>Connoisseur</span>
