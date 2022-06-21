25b7a7e87afe066f375e437a6e1fb09649b9b579
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-06-21 10:41:23 +0430

    banner style for mobile screen size

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 function Banner(props) {
     return (
-        <div>
-            <h1 className='text-5xl text-black font-bold'>
-                <span>Coffee</span>
-                <span className='text-violet-900 font-semibold'>Connoisseur</span>
+        <div className='flex flex-col mx-auto space-x-2 p-6 '>
+            <h1 className='text-3xl text-white font-bold'>
+                <span className='px-1'>Coffee</span>
+                <span className='text-violet-900'>Connoisseur</span>
             </h1>
-            <p>Discover your local coffe shops!</p>
-            <button onClick={props.handleOnClick} className='py-2 px-4 text-white bg-violet-800 rounded-md shadow-xl hover:bg-white hover:text-black'>{props.buttonText}</button>
+            <p className='mx-2'>Discover your local coffe shops!</p>
+            <button onClick={props.handleOnClick} className='py-2 px-4 text-white mt-6 bg-violet-800 rounded-md shadow-xl hover:bg-white hover:text-black'>{props.buttonText}</button>
         </div>
     )
 }
