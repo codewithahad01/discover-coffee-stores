4dfb69394bea9ef19081b8678817277da60d2a0a
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-07-01 10:04:44 +0430

    style components added

--- components/Card.jsx
+++ components/Card.jsx
@@ -6,15 +6,16 @@ import cls from 'classnames'
 
 function Card(props) {
     return (
+        
         <div className=' justify-center items-center my-10 mx-4  '>
             <Link href={props.href} >
                 <a>
                     <div className={cls("glass", "p-8 rounded-lg shadow-2xl mx-auto md:")}>
-                        <div>
+                        <div className='text-2xl text-blue-800 font-bold my-4 md:text-2xl '>
                             <h2>{props.name}</h2>
                         </div>
-                        <div>
-                            <Image src={props.imgUrl} alt="header img" width={260} height={160} />
+                        <div className='m-10 rounded-lg '>
+                            <Image className='rounded-lg md:w-auto' src={props.imgUrl} alt="header img"  width={460} height={360} />
                         </div>
                     </div>
                 </a>
