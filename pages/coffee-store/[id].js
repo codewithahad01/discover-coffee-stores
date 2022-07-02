980ccdb1e78e955f5d93b10c0f20317c7ef45fe5
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-07-02 18:58:15 +0430

    static props added

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,15 +1,27 @@
 import React from 'react'
 import {useRouter} from 'next/router'
 import Link from 'next/link'
+import CoffeeStoresData from '../../data/coffee-stores.json'
+
+export function getStaticProps(staticProps) {
+    const params = staticProps.params
+    return {
+        props: {
+            CoffeeStore: CoffeeStoresData.find(CoffeeStore => {
+                return CoffeeStore.id === 0 
+            }),
+        }
+    }
+}
 
 const CoffeeStore = () => {
     const router = useRouter()
-    
+    console.log("router", router)
     return (
         <div>
-            <h1 className='text-4xl'>Coffee Store {router.query.id}</h1>
-            <Link href='/'><a className='text-4xl underline'>Back to home</a></Link>
-            <Link href='/coffee-store/dynamic'><a className='text-4xl underline'>go to page dynamic     </a></Link>
+            <h1 className='text-4xl'>Coffee Store {router.query.id}</h1><br />
+            <Link href='/'><a className='text-4xl underline'>Back to home</a></Link><br />
+            <Link href='/coffee-store/dynamic'><a className='text-4xl underline'>go to page dynamic</a></Link>
         </div>
     )
 }
