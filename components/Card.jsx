cae3b43ce6c6d81625f1e136a59df30dee1758c7
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-07-02 14:37:16 +0430

    another component added

--- components/Card.jsx
+++ components/Card.jsx
@@ -10,11 +10,12 @@ function Card(props) {
         <div className=' justify-center items-center my-10 mx-4  '>
             <Link href={props.href} >
                 <a>
-                    <div className={cls("glass", "p-8 rounded-lg shadow-2xl mx-auto md:")}>
-                        <div className='text-2xl text-blue-800 font-bold my-4 md:text-2xl '>
+                    <div className={cls("glass", "p-2 rounded-lg shadow-2xl mx-auto md:")}>
+                        <div className='text-2xl text-blue-800 font-bold my-4 md:text-2xl text-center'>
                             <h2>{props.name}</h2>
                         </div>
-                        <div className='m-10 rounded-lg '>
+                        
+                        <div className='m-10 rounded-lg flex justify-center items-center'>
                             <Image className='rounded-lg md:w-auto' src={props.imgUrl} alt="header img"  width={460} height={360} />
                         </div>
                     </div>
